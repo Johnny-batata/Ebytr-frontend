2d7c9dfa8774bacb25e83c1993c94be8e78fa114
jaarguello <johnny.arguello007@gmail.com>
2021-11-22 18:05:27 -0300

    Updates

---
+++ precache-manifest.c26c42a9c91b2ead7fdf0050f46aa6e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f622174b97f1521cccc95b8262376d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "129ee5a0b33c8b03c00c",
+    "url": "/static/css/main.d7c92d86.chunk.css"
+  },
+  {
+    "revision": "66c7170795a8f00b8101",
+    "url": "/static/js/2.f7560e83.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/static/js/2.f7560e83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "129ee5a0b33c8b03c00c",
+    "url": "/static/js/main.fb1f128d.chunk.js"
+  },
+  {
+    "revision": "7b78667e2ba684d6fa51",
+    "url": "/static/js/runtime-main.c44f9cbc.js"
+  },
+  {
+    "revision": "7562d48c565c79043f508afe4dd23e9e",
+    "url": "/static/media/iconedoApp3.7562d48c.jpg"
+  }
+]);
